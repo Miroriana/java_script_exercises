fcfff22cf4d0893a780f8b8b034f21364acda923
miroriana <shumbushoihiclaudia@gmail.com>
2023-11-17 10:01:37 +0100

    before

--- 15th_question.js
+++ 15th_question.js
@@ -3,3 +3,11 @@
 // Create a function to reverse the order of the 
 // elements inside the given array.
 
+function reversed(array){
+    var reversedarray = array.reverse('');
+    console.log(reversedarray);
+}
+var array = [1,3,4,3,7,8,0,12,19];  
+reversed(array);
+
+
